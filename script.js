22324c93edc6baeef75e204f1f5b5af3b3113f1d
Kraten06 <photo.edit.kat@gmail.com>
2024-11-08 19:16:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+function showit(var1){
+// Hide the one div that is shown
+document.getElementById('divvy1').style.display = 'none';
+document.getElementById('divvy2').style.display = 'none';
+document.getElementById('divvy3').style.display = 'none';
+document.getElementById('divvy4').style.display = 'none';
+// show the one div that you sent to the function 
+document.getElementById(var1).style.display = 'block';
+}
